8886336bcd511f8abff54768da1737e6ede50bb2
Alexis Araujo <ala1289@gmail.com>
2019-07-23 08:40:46 -0500

    funciones

---
+++ clase4.js
@@ -0,0 +1,9 @@
+var nombre = 'Alexis', edad = 29
+
+function imprimirEdad (n, e) {
+  console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Eric', 24)
+imprimirEdad('Dario', 27)
