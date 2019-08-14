0e43154ee6d4f037e41ac25edfd19d7a43e667d6
Alexis Araujo <ala1289@gmail.com>
2019-08-14 13:35:07 -0500

    Manejando modulos externos con NPM y require()

--- nodejsBasicoCurso/index.js
+++ nodejsBasicoCurso/index.js
@@ -6,9 +6,19 @@
 const math = require("./math") 
 const greet = require("./greetings")
 const hello = require("./greetings/hello")
+const cowsay = require("cowsay")
+
 
 console.log(math.add(4,5));
 
 console.log(greet.greet("alexis"));
 console.log(hello.sayHello("alexis"));
+
+// Uso o manejando modulos externos con NPM y require()
+console.log(cowsay.say({
+  text: "Alexis",
+  e: "^^",
+  T: "U"
+}));
+
 
