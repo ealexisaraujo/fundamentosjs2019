b5e5390e2e5543aa5f8d479e8e446fa08dc09a15
Alexis Araujo <ala1289@gmail.com>
2019-08-23 16:20:11 -0500

    Estructuras repetitivas: while

---
+++ clase14.js
@@ -0,0 +1,36 @@
+// Inicio de clase estructuras repetitivas: while
+
+var alexis = {
+  nombre: 'Alexis',
+  edad: 28,
+  peso: 75
+}
+
+console.log(`Al inicio del año ${alexis.nombre} pesa ${alexis.peso}kg`)
+
+// Convertir funcion en arrow function
+// function aumentarDePeso(persona) {
+//   return persona.peso += 200
+// }
+const INCREMENTO_PESO = 0.2
+const DIAS_DEL_ANO = 365
+const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
+const adelgazar = persona => persona.peso -= INCREMENTO_PESO
+const comeMucho = () => Math.random() < 0.30
+const realizaDeporte = () => Math.random() < 0.40 
+
+const META = alexis.peso - 3
+var diasAdelgazar = 0
+
+while (alexis.peso > META) {
+  if (comeMucho()) {
+    //aumentardepeso
+    aumentarDePeso(alexis)
+  }
+  if (realizaDeporte()) {
+    adelgazar(alexis)
+  }
+  diasAdelgazar += 1
+}
+
+console.log(`Pasaron ${diasAdelgazar} dias hasta que ${alexis.nombre} adelgazo 3kg`)
