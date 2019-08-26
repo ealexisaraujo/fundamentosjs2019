117fd20bcf9de337e2cade2652ca13a6789aed22
Alexis Araujo <ala1289@gmail.com>
2019-08-26 09:40:53 -0500

    Transformar un array

--- clase17.js
+++ clase17.js
@@ -16,18 +16,35 @@ var martin = {
   altura: 1.85
 }
 
+var paula = {
+  nombre: 'Paula',
+  apellido: 'Barros',
+  altura: 1.65
+}
+
 const esAlta = ({altura}) => altura > 1.80
 
 const esBaja = (persona) => persona.altura < 1.80
 
-var personas = [alexis, alan, martin]
+var personas = [alexis, alan, martin, paula]
 
 var personasAltas = personas.filter(esAlta)
 var personasBajas = personas.filter(esBaja)
 
-// var personasAltas = personas.filter(function (persona) {
-//   return persona.altura > 1.80
-// })
 
-console.log(personasAltas)
- 
+
+const pasarAlturaCMS = persona => ({
+  ...persona,
+  altura: persona.altura * 100
+})
+
+// {
+//   return {
+//     ...persona,
+//     altura: persona.altura * 100
+//   }
+// }
+
+var personasCms = personas.map(pasarAlturaCMS)
+console.log(personasCms);
+console.log(personas);
