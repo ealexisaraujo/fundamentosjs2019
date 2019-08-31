3ec25fa3e2bcf206ed60f59b54566e7e3a04fd2e
Alexis Araujo <ala1289@gmail.com>
2019-08-30 22:08:02 -0500

    Haciendo multiples requests

---
+++ clase25.js
@@ -0,0 +1,18 @@
+// Asincronismo
+// Clase: Haciendo multiples requests
+
+function obtenerPersonaje(id) {
+
+  const API_URL = 'https://swapi.co/api/'
+  const PEOPLE_URL = 'people/:id/'
+
+  fetch(`${API_URL}${PEOPLE_URL.replace(':id',id)}`)
+        .then(response => response.json())
+        .then(persona => {
+          console.log(`Hola soy ${persona.name}`)
+        })
+}
+
+  for (let i = 1; i < 15  ; i++) {
+    obtenerPersonaje(i)
+  }
