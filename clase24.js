63b614327019d74da2e891f7703267edda999396
Alexis Araujo <ala1289@gmail.com>
2019-08-30 21:57:14 -0500

    Callbacks

---
+++ clase24.js
@@ -0,0 +1,12 @@
+// Asincronismo
+// Clase: Callbacks
+const API_URL = 'https://swapi.co/api/'
+    const PEOPLE_URL = 'people/:id/'
+
+      for (let i = 1; i < 15  ; i++) {
+        fetch(`${API_URL}${PEOPLE_URL.replace(':id',i)}`)
+        .then(response => response.json())
+        .then(persona => {
+          console.log(`Hola soy ${persona.name}`)
+        })
+      }
