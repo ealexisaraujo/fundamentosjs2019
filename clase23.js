c09c05541417979b0a58f4ebc5c9e79303576c23
Alexis Araujo <ala1289@gmail.com>
2019-08-30 21:32:30 -0500

    Como funciona el tiempo en javascript

---
+++ clase23.js
@@ -0,0 +1,11 @@
+// Asincronismo
+// Clase: Como funciona el tiempo en javascript
+// console.log('a');
+// setTimeout(() => {
+//   console.log('b');
+// }, 0);
+setTimeout(() => {
+  console.log('d');
+}, 2000);
+for (var i = 0; i < 10000000000; i++) {
+}
