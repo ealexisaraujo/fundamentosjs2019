9bc241b02f148165966a2a62a29fb0bb07b89e5d
Alexis Araujo <ala1289@gmail.com>
2019-09-02 11:21:43 -0500

    Promesas encadenadas

--- clase29.js
+++ clase29.js
@@ -7,7 +7,7 @@ function obtenerPersonaje(id) {
 
   return fetch(`${API_URL}${PEOPLE_URL.replace(":id", id)}`)
     .then(response => response.json())
-    .catch(() => console.log(`Error al obtener el personaje ${id.name}`));
+    .catch(() => console.log(`Error al obtener el personaje ${id}`));
 }
 
 obtenerPersonaje(1)
@@ -21,4 +21,20 @@ obtenerPersonaje(1)
   })
   .then(personaje => {
     console.log(`El personaje 3 es ${personaje.name}`);
+    return obtenerPersonaje(4);
+  })
+  .then(personaje => {
+    console.log(`El personaje 4 es ${personaje.name}`);
+    return obtenerPersonaje(5);
+  })
+  .then(personaje => {
+    console.log(`El personaje 5 es ${personaje.name}`);
+    return obtenerPersonaje(6);
+  })
+  .then(personaje => {
+    console.log(`El personaje 6 es ${personaje.name}`);
+    return obtenerPersonaje(7);
+  })
+  .then(personaje => {
+    console.log(`El personaje 7 es ${personaje.name}`);
   });
